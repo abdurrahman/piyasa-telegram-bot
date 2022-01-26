11f16224b12a84bd30c5bb453639f1a29ed31520
Abdurrahman Işık <isikabdurrahman@yahoo.com>
2022-01-26 22:14:05 +0200

    update config

--- config.py
+++ config.py
@@ -1,5 +1,5 @@
 TOKEN='1171776040:AAFG99v-MA8KEQCT7tTeG9onihxeLk0xi-8'
-NGROK_URL='http://a88e-2001-7d0-84a6-4100-bc74-3e45-f09a-8d53.ngrok.io'
+NGROK_URL='https://a88e-2001-7d0-84a6-4100-bc74-3e45-f09a-8d53.ngrok.io'
 HEROKU_APP='https://piyasatr-bot.herokuapp.com'
 BASE_TELEGRAM_API_URL='https://api.telegram.org/bot{}'.format(TOKEN)
 LOCAL_WEBHOOK_ENDPOINT='{}/webhook'.format(NGROK_URL)
