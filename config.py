5f6a63044b9b21a5e3faa5d5f01fc9972a717fd2
Abdurrahman Işık <isikabdurrahman@yahoo.com>
2022-02-01 21:04:35 +0200

    update config

--- config.py
+++ config.py
@@ -5,5 +5,5 @@ BASE_TELEGRAM_API_URL='https://api.telegram.org/bot{}'.format(TOKEN)
 LOCAL_WEBHOOK_ENDPOINT='{}/webhook'.format(NGROK_URL)
 REMOTE_WEBHOOK_ENDPOINT='{}/webhook'.format(HEROKU_APP)
 
-TELEGRAM_INIT_WEBHOOK_URL = '{}/setWebhook?url={}'.format(BASE_TELEGRAM_API_URL, LOCAL_WEBHOOK_ENDPOINT)
+TELEGRAM_INIT_WEBHOOK_URL = '{}/setWebhook?url={}'.format(BASE_TELEGRAM_API_URL, REMOTE_WEBHOOK_ENDPOINT)
 TELEGRAM_SEND_MESSAGE_URL = BASE_TELEGRAM_API_URL + '/sendMessage?chat_id={}&text={}'
